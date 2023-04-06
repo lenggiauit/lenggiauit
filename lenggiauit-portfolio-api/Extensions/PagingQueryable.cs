fce227e19736e8cd4964fc700d838f5853b4b6ab
Lenggiauit <lenggiauit@gmail.com>
2023-04-05 21:20:26 -0400

    remove private key

--- lenggiauit-portfolio-api/Extensions/PagingQueryable.cs
+++ lenggiauit-portfolio-api/Extensions/PagingQueryable.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Data; 
 using System.Linq.Dynamic.Core;
 using Lenggiauit.API.Domain.Helpers;
+using System.Linq.Expressions;
+using System;
 
 namespace Lenggiauit.API.Extensions
 {
@@ -26,7 +28,59 @@ namespace Lenggiauit.API.Extensions
             {
                 query = query.Skip((requestMetaData.Paging.Index - 1) * requestMetaData.Paging.Size).Take(requestMetaData.Paging.Size);
             }
+
+            //if (requestMetaData?.Filters != null)
+            //{
+            //    foreach (var f in requestMetaData.Filters)
+            //    {
+            //        query = query.Where(BuildPredicate<T>(f.Field, f.Comparison, f.Value));
+            //    }
+            //}
+
             return query;
         }
+
+
+        public static Expression<Func<T, bool>> BuildPredicate<T>(string fieldName, string comparison, string value)
+        {
+            var parameter = Expression.Parameter(typeof(T));
+            var left = fieldName.Split('.').Aggregate((Expression)parameter, Expression.PropertyOrField);
+            var body = MakeComparison(left, comparison, value);
+            return Expression.Lambda<Func<T, bool>>(body, parameter);
+        }
+
+        private static Expression MakeComparison(Expression left, string comparison, string value)
+        {
+            var constant = Expression.Constant(value, left.Type);
+            switch (comparison)
+            {
+                case "==":
+                    return Expression.MakeBinary(ExpressionType.Equal, left, constant);
+                case "!=":
+                    return Expression.MakeBinary(ExpressionType.NotEqual, left, constant);
+                case ">":
+                    return Expression.MakeBinary(ExpressionType.GreaterThan, left, constant);
+                case ">=":
+                    return Expression.MakeBinary(ExpressionType.GreaterThanOrEqual, left, constant);
+                case "<":
+                    return Expression.MakeBinary(ExpressionType.LessThan, left, constant);
+                case "<=":
+                    return Expression.MakeBinary(ExpressionType.LessThanOrEqual, left, constant);
+                case "Contains":
+                case "StartsWith":
+                case "EndsWith":
+                    if (value is string)
+                    {
+                        return Expression.Call(left, comparison, Type.EmptyTypes, constant);
+                    }
+                    throw new NotSupportedException($"Comparison operator '{comparison}' only supported on string.");
+                default:
+                    throw new NotSupportedException($"Invalid comparison operator '{comparison}'.");
+            }
+        }
+
     }
+
+
+
 }
